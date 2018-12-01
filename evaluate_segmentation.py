b3b4dd8f9c59e4053a10de3911632f10db490a92
RJ Nowling <rnowling@gmail.com>
2018-12-01 11:15:51 -0600

    Calculate per-image dice score, accuracy for binary labels

--- evaluate_segmentation.py
+++ evaluate_segmentation.py
@@ -43,19 +43,26 @@ def train_and_predict(args):
     imgs_mask_test = imgs_mask_test.astype('float32')
     imgs_mask_test /= 255.  # scale masks to [0, 1]
     imgs_mask_test = np.around(imgs_mask_test)
+    test_labels = [mask_img.flatten().max() > 0 \
+                   for mask_img in imgs_mask_test]
+
     
-    pred_masks = np.load(os.path.join(args.input_dir, 'imgs_pred_mask_test.npy'))
+    pred_masks = np.load(os.path.join(args.input_dir,
+                                      'imgs_pred_mask_test.npy'))
 
     pred_masks = pred_masks.astype('float32')
     scaled = pred_masks / 255.
+    pred_labels = [np.around(scaled.flatten()).max() > 0 \
+                   for mask_img in imgs_mask_test]
+    
+    dice = [np_dice_coef(imgs_mask_test[i],
+                         scaled[i]) \
+            for i in xrange(len(imgs_mask_test))]
 
-    dice = np_dice_coef(imgs_mask_test,
-                        scaled)
+    acc = accuracy_score(test_labels,
+                         pred_labels)
 
-    acc = accuracy_score(scaled.reshape(-1),
-                         imgs_mask_test.reshape(-1))
-
-    print("Dice coefficient:", dice)
+    print("Dice coefficient:", np.mean(dice), np.std(dice))
     print("Accuracy:", acc)
 
 def parseargs():
